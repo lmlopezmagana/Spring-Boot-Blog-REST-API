f3fe2a147ea0e0ff5d9e0e7587bcf38373d1f5da
Manu <manufernarjona1@gmail.com>
2022-01-27 21:30:02 +0100

    terminar postRepository

--- src/test/java/com/sopromadze/blogapi/repository/PostRepositoryTest.java
+++ src/test/java/com/sopromadze/blogapi/repository/PostRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.sopromadze.blogapi.repository;
 
+import com.sopromadze.blogapi.model.Category;
 import com.sopromadze.blogapi.model.Post;
 import com.sopromadze.blogapi.model.role.RoleName;
 import com.sopromadze.blogapi.model.user.User;
@@ -34,22 +35,65 @@ class PostRepositoryTest {
     TestEntityManager testEntityManager;
 
     @Test
+    /*preguntar luismi*/
     void findByCreatedBy() {
-
         UserPrincipal userP = new UserPrincipal(3L,"Manuel", "Fernández", "ManuFer", "manufer@gmail.com", "123456789", Collections.singleton(new SimpleGrantedAuthority(RoleName.ROLE_USER.toString())));
 
         Post post1 = new Post();
+        post1.setCreatedBy(3L);
+        post1.setCreatedAt(Instant.now());
+        post1.setUpdatedAt(Instant.now());
+        testEntityManager.persist(post1);
+
         Post post2 = new Post();
+        post2.setCreatedBy(3L);
+        post2.setCreatedAt(Instant.now());
+        post2.setUpdatedAt(Instant.now());
+        testEntityManager.persist(post2);
+
+
         Post post3 = new Post();
+        post3.setCreatedBy(3L);
+        post3.setCreatedAt(Instant.now());
+        post3.setUpdatedAt(Instant.now());
+        testEntityManager.persist(post3);
+
 
         Pageable pageable = PageRequest.of(1, 1, Sort.Direction.DESC, "createdAt");
-        Iterable<Post> posts = postRepository.findByCreatedBy(3L, pageable);
-        assertThat(posts).hasSize(3).contains(post1, post2, post3);
 
+        Iterable<Post> posts = postRepository.findByCreatedBy(3L, pageable);
+        assertThat(posts).hasSize(1).contains(post2);
     }
 
     @Test
+    /*falta terminar*/
     void findByCategory() {
+        Category cat = new Category();
+        cat.setId(1L);
+
+        Post post1 = new Post();
+        post1.setId(5L);
+        post1.setCategory(cat);
+        post1.setCreatedAt(Instant.now());
+        post1.setUpdatedAt(Instant.now());
+
+        Post post2 = new Post();
+        post1.setId(2L);
+        post2.setCategory(cat);
+        post2.setCreatedAt(Instant.now());
+        post2.setUpdatedAt(Instant.now());
+
+        Post post3 = new Post();
+        post1.setId(3L);
+        post3.setCategory(cat);
+        post3.setCreatedAt(Instant.now());
+        post3.setUpdatedAt(Instant.now());
+
+        Pageable pageable = PageRequest.of(1, 1, Sort.Direction.DESC, "createdAt");
+        Iterable<Post> posts = postRepository.findByCategory(1L, pageable);
+
+        assertThat(posts).contains(post1, post2, post3);
+
     }
 
     @Test

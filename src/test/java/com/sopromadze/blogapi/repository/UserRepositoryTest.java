399e696e5567d4bb17cd8f2aa54c081ac1be7d59
Supherum <supherum@gmail.com>
2022-02-01 19:25:14 +0100

    jacoco 0.8.7

--- src/test/java/com/sopromadze/blogapi/repository/UserRepositoryTest.java
+++ src/test/java/com/sopromadze/blogapi/repository/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import com.sopromadze.blogapi.model.user.User;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.sopromadze.blogapi.security.UserPrincipal;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;

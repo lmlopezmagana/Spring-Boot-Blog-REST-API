c8212042de69688c3529260e3a8eccf601c59196
Cynthia Labrador <labrador.cacyn21@triana.salesianos.edu>
2022-02-01 13:25:19 +0100

    Agregamos cambios en los tests de los repositorios

--- src/test/java/com/sopromadze/blogapi/repository/RoleRepositoryTest.java
+++ src/test/java/com/sopromadze/blogapi/repository/RoleRepositoryTest.java
@@ -1,13 +1,18 @@
 package com.sopromadze.blogapi.repository;
 
+import com.sopromadze.blogapi.model.role.Role;
 import com.sopromadze.blogapi.model.role.RoleName;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
@@ -18,15 +23,22 @@ class RoleRepositoryTest {
     @Autowired
     RoleRepository roleRepository;
 
-    private static RoleName role;
+    @Autowired
+    TestEntityManager testEntityManager;
+
+    private static Role role;
 
     @BeforeEach
     void setUp() {
-        role = RoleName.ROLE_ADMIN;
+
+        role = new Role();
+        role.setName(RoleName.ROLE_ADMIN);
     }
 
     @Test
     void findByName() {
-        assertNotNull(roleRepository.findByName(role));
+        testEntityManager.persist(role);
+        assertNotNull(roleRepository.findByName(role.getName()));
+        assertEquals(Optional.of(role),roleRepository.findByName(RoleName.ROLE_ADMIN));
     }
 }
